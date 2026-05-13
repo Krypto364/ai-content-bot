d50bea3a31314b233d8729c3a11fcdb8fbdc4d34
Krypto364 <amanthakur1419999@gmail.com>
2026-05-13 22:19:15 +0530

    Update image_fetcher.py

--- src/image_fetcher.py
+++ src/image_fetcher.py
@@ -5,11 +5,19 @@ from PIL import Image
 from io import BytesIO
 
 def fetch_image(query, filename):
-    url = f"https://source.unsplash.com/720x1280/?{query}"
+
+    # Use stable image source
+    url = f"https://picsum.photos/720/1280"
 
     response = requests.get(url)
 
-    img = Image.open(BytesIO(response.content)).convert("RGB")
+    if response.status_code != 200:
+        raise Exception("Image download failed")
+
+    try:
+        img = Image.open(BytesIO(response.content)).convert("RGB")
+    except:
+        raise Exception("Invalid image received")
 
     path = f"assets/temp/{filename}.jpg"
     img.save(path, "JPEG")
